17dff0e3be9f608f902134a4906e8d2d36d0519c
mattg07 <matiasmgodoy00@gmail.co.com>
2024-06-21 17:24:48 +0200

    wrong export 4 commit

--- src/libs/auth.ts
+++ src/libs/auth.ts
@@ -1,15 +1,16 @@
-import NextAuth, { NextAuthOptions } from "next-auth"
+import NextAuth, { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-export const authOptions : NextAuthOptions = {
-    secret: process.env.SECRET,
-providers: [
-  GoogleProvider({
-    clientId: process.env.GOOGLE_CLIENT_ID as string,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-  })
-]};
+export const authOptions: NextAuthOptions = {
+  secret: process.env.SECRET,
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    })
+  ]
+};
 
 const handler = NextAuth(authOptions);
 
-export { handler as GET, handler as POST }
+export { handler as GET, handler as POST };
